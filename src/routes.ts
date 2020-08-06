4e9598dfbf5000007dbc3a62063f3c06773f5ead
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-08-05 23:51:15 -0300

    API finished

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ const connectionsController = new ConnectionsController();
 routes.post("/classes", classesControllers.create);
 routes.get("/classes", classesControllers.index);
 
-routes.post('/connections', connectionsController.create);
-routes.get('/connections', connectionsController.index);
+routes.post("/connections", connectionsController.create);
+routes.get("/connections", connectionsController.index);
 
 export default routes;
